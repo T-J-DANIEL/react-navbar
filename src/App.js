cd88929f94ecf8e2c12da7d8e5b3d933fe76f063
T-J-DANIEL <51197833+T-J-DANIEL@users.noreply.github.com>
2021-11-30 15:50:04 +0000

    full functionality and majority of css complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useRef, useEffect } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faTwitter,
+  faGithub,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
+// ctrl+space see whats available
 function App() {
+  const [hideMenu, setHideMenu] = useState(true)
+  const currentRef = useRef(null)
+  const containerRef = useRef(null)
+  useEffect(() => {
+    if (hideMenu) {
+      containerRef.current.style.height = "0"
+    } else {
+      const listSize = currentRef.current.getBoundingClientRect().height
+      containerRef.current.style.height = `calc(${listSize}px + 2em)`
+    }
+  }, [hideMenu])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={`App-header`}>
+        <h1 className="header-title">
+          Tim's<span className="title-color">Website</span>
+        </h1>
+        <div className="menu-button-container">
+          <div
+            className="menu-button"
+            onClick={() => {
+              setHideMenu((prevValue) => !prevValue)
+            }}
+          >
+            {/* menu button bars */}
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <nav ref={containerRef}>
+          <ul ref={currentRef}>
+            <li>
+              <a href="_#">Home</a>
+            </li>
+            <li>
+              <a href="_#">About</a>
+            </li>
+            <li>
+              <a href="_#">Projects</a>
+            </li>
+            <li>
+              <a href="_#">Contact</a>
+            </li>
+            <li>
+              <a href="_#">Profile</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="socials-container">
+          <ul className="socials">
+            <li>
+              <FontAwesomeIcon icon={faTwitter} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faGithub} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faFacebook} />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faInstagram} />
+            </li>
+          </ul>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
